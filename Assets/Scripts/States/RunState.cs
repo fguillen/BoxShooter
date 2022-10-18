ef34650bf5f027a9c7ba629a98a090db147a85cd
Fernando Guillen <fguillen.mail@gmail.com>
2022-10-18 18:43:55 +0200

    Move discrete movement

--- Assets/Scripts/States/RunState.cs
+++ Assets/Scripts/States/RunState.cs
@@ -8,7 +8,7 @@ public class RunState : State
 {
     [SerializeField] private UnityEvent OnStep;
 
-    Vector2 nextPosition;
+    Vector2 movementDirection;
 
     public override StateType Type()
     {
@@ -18,6 +18,7 @@ public class RunState : State
     protected override void EnterState()
     {
         agent.animationManager.PlayAnimation(AnimationType.run);
+        movementDirection = agent.movementData.movementDirectionDiscrete;
         MoveToCell();
     }
 
@@ -30,8 +31,8 @@ public class RunState : State
     {
         DOTween.Kill(agent.transform);
 
-        nextPosition = GridUtils.CellPositionInDirection(agent.transform.position, agent.movementData.movementDirectionDiscrete);
-        Debug.Log($"MoveToCell({nextPosition})");
+        Vector2 nextPosition = GridUtils.CellPositionInDirection(agent.transform.position, movementDirection);
+        // Debug.Log($"MoveToCell({nextPosition})");
         agent.transform
             .DOMove(nextPosition, agent.agentData.maxSpeed)
             .OnComplete(MovementFinished)
@@ -42,34 +43,45 @@ public class RunState : State
 
     void MovementFinished()
     {
-        if(agent.movementData.agentMovement.magnitude == 0f)
+        if(movementDirection.magnitude == 0f)
             agent.stateManager.TransitionToState(StateType.Idle);
         else
-            MoveToCell();
+            NextCell();
     }
 
-    protected override void HandleAnimationAction()
-    {
-        Debug.Log($"HandleAnimationAction()");
-        OnStep?.Invoke();
-    }
+    // protected override void HandleAnimationAction()
+    // {
+    //     Debug.Log($"HandleAnimationAction()");
+    //     OnStep?.Invoke();
+    // }
 
-    protected override void HandleAttack()
-    {
-        Debug.Log($"HandleAttack()");
-        if(agent.weaponManager.CanAttack())
-            agent.stateManager.TransitionToState(StateType.Attack);
-    }
+    // protected override void HandleAttack()
+    // {
+    //     Debug.Log($"HandleAttack()");
+    //     if(agent.weaponManager.CanAttack())
+    //         agent.stateManager.TransitionToState(StateType.Attack);
+    // }
 
-    protected override void HandleHitted(Vector2 point)
-    {
-        Debug.Log($"HandleHitted()");
-        agent.stateManager.TransitionToState(StateType.Hit);
-    }
+    // protected override void HandleHitted(Vector2 point)
+    // {
+    //     Debug.Log($"HandleHitted()");
+    //     agent.stateManager.TransitionToState(StateType.Hit);
+    // }
 
     protected override void HandleMovement(Vector2 movement)
     {
-        if(movement.magnitude > 0f)
+        // if(movement.x == movementDirection.x || movement.y == movementDirection.y)
+        // {
+
+        // }
+
+        movementDirection = agent.movementData.movementDirectionDiscrete;
+        NextCell();
+    }
+
+    void NextCell()
+    {
+        if(movementDirection.magnitude > 0f)
             MoveToCell();
     }
 }
